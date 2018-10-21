69fedfdf8a87065dcb7cbce168e9c28129479d36
atfieldson <37640441+atfieldson@users.noreply.github.com>
2018-10-21 10:37:58 -0500

    orbit controls set to my preferences
    Using controls.maxDistance.minDistance and contrlols.maxPolarAngle/minPolarAngle, the orbit controls are keeping the handle completely in view at all times.

--- public/client.js
+++ public/client.js
@@ -18,7 +18,7 @@ myCanvas.height = 500; //new
 renderer.setViewport(0, 0, 1000, 1000)
 
 //using clientWidth and height below is what allows the proportions to remain accurate, as distinct
-//using window.innerWidth and height.
+//from using window.innerWidth and height.
 let camera = new THREE.PerspectiveCamera(35, myCanvas.clientWidth/myCanvas.clientHeight, 2, 1000)
 controls = new THREE.OrbitControls(camera, renderer.domElement);
 
@@ -30,6 +30,14 @@ let scene = new THREE.Scene();
 
 controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.target.set(0, 0, 0)
+controls.maxDistance = 120;
+controls.minDistance = 60;
+
+//set vertical orbit limitations
+controls.maxPolarAngle = Math.PI/1.9;
+controls.minPolarAngle = Math.PI/2.15;
+//set how far you can zoom in 
+
 
 //lights
 let light = new THREE.AmbientLight(0xffffff, .3);
@@ -42,33 +50,51 @@ scene.add(light1);
 let light2 = new THREE.PointLight(0xffffff, .6);
 light2.position.set(50, 15, 15)
 scene.add(light2);
+//end lights
 
-//WhipWorks logo
-let WWLogo = new THREE.TextureLoader().load("ww.jpg");
 
 //Backgrounds
-let geoLeftWW = new THREE.PlaneBufferGeometry(50, 50, 8, 8);
-let matLeftWW = new THREE.MeshPhongMaterial({ color: 0xffffff, map: WWLogo });
-let plane = new THREE.Mesh(geoLeftWW, matLeftWW);
-plane.position.set(-35, 0, -15)
-plane.rotation.y = -12;
+let WWLogo = new THREE.TextureLoader().load("ww.jpg");
+let geo0WW = new THREE.PlaneBufferGeometry(50, 50, 8, 8);
+let mat0WW = new THREE.MeshPhongMaterial({ color: 0xffffff, map: WWLogo });
+let plane0 = new THREE.Mesh(geo0WW, mat0WW);
+plane0.position.set(0, 0, -60)
+plane0.rotation.y = 0;
 
-let geoRightWW = new THREE.PlaneBufferGeometry(50, 50, 8, 8);
-let matRightWW = new THREE.MeshPhongMaterial({ color: 0xffffff, map: WWLogo });
-let plane1 = new THREE.Mesh(geoRightWW, matRightWW);
-plane1.position.set(35, 0, -15)
-plane1.rotation.y = 12;
+let Indy = new THREE.TextureLoader().load("indy.jpg");
+let geo1WW = new THREE.PlaneBufferGeometry(50, 50, 8, 8);
+let mat1WW = new THREE.MeshPhongMaterial({ color: 0xffffff, map: Indy });
+let plane1 = new THREE.Mesh(geo1WW, mat1WW);
+plane1.position.set(60, 0, 0)
+plane1.rotation.y = -Math.PI/2;
 
+let CatWhip = new THREE.TextureLoader().load("catWhip.jpg");
+let geo2WW = new THREE.PlaneBufferGeometry(50, 50, 8, 8);
+let mat2WW = new THREE.MeshPhongMaterial({ color: 0xffffff, map: CatWhip });
+let plane2 = new THREE.Mesh(geo2WW, mat2WW);
+plane2.position.set(-60, 0, 0)
+plane2.rotation.y = Math.PI/2;
 
+let SaberWhip = new THREE.TextureLoader().load("saberWhip.jpg");
+let geo3WW = new THREE.PlaneBufferGeometry(50, 50, 8, 8);
+let mat3WW = new THREE.MeshPhongMaterial({ color: 0xffffff, map: SaberWhip });
+let plane3 = new THREE.Mesh(geo3WW, mat3WW);
+plane3.position.set(0, 0, 60)
+plane3.rotation.y = Math.PI;
 
-scene.add(plane, plane1);
+scene.add(plane0, plane1, plane2, plane3);
+//end backgrounds
 
+//render
 requestAnimationFrame(render);
 
 function render() {
 	renderer.render(scene, camera);
 	requestAnimationFrame(render);
+
+	controls.update();
 }
+//end render
 
 
 app.controller('HandleDesignController', [function () {
